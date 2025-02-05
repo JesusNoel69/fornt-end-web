3dfbf3b3f5a2af870beb62a57c3a3cce0a2a9b50
Jesus <jnoel883@gmail.com>
2025-02-04 20:56:26 -0700

    changing router file.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { PrincipalBoardComponent } from './components/principal-board/principal-board.component';
 import { ScrumBoardComponent } from './components/scrum-board/scrum-board.component';
 import { SprintBoardComponent } from './components/sprint-board/sprint-board.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 export const routes: Routes = [
     {path: 'home', component: PrincipalBoardComponent},
     {path: 'sprints', component: SprintBoardComponent},
     {path: 'scrum', component: ScrumBoardComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+
     { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
